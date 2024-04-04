1977f1d50173a10da4b528c72206dbbb6c36018e
Vinicius Montes <viniciusmontes@outlook.com.br>
2024-04-04 16:03:29 -0300

    Implemented exceptions, validation and security infrastructure

---
+++ src/main/java/com/devsuperior/movieflix/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.devsuperior.movieflix.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devsuperior.movieflix.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	
+	User findByEmail(String email);
+
+}
